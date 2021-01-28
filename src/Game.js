cff1491b8143e359457ef5edf9cdaf542d5b5cb7
Vimpy Jain <vimpy@techuz.com>
2021-01-28 17:30:00 +0530

    changes done

--- src/Game.js
+++ src/Game.js
@@ -4,14 +4,10 @@ import Board from './Board';
 const calculateWinner = (squares) => {
   // Operation
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 4, 2],
+    [0, 4, 6],
+    [2, 4, 8],
+    [6, 4, 8],
   ];
   for (let i=0;i<lines.length;i++){
     let [a, b, c] = lines[i];
